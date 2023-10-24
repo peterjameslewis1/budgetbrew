f272006243cd07dce6c3fb41e7861abab40d6cbc
Peter Lewis <peterjameslewis4@hotmail.com>
2023-10-24 12:42:42 +0100

    add text regarding accurate submittions

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -94,6 +94,7 @@ export default function Home() {
         <div className='intro'>
             <h4><strong>BudgetBrews</strong> is here to help you find the most affordable pubs in town.</h4>
             <p>No signup required. Search and submit your local pub prices.</p>
+            <p>Please be as accurate as possible when submitting data. Your contributions are vital to our success.</p>
             <a href='mailto:peterjameslewis4@hotmail.com'>Feedback</a>
             <a target="_blank" href='https://www.buymeacoffee.com/peterjamesr' >Buy me a beer <FontAwesomeIcon icon={faArrowRight} /></a>
         </div>

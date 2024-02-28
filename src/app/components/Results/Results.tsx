0abe24bc0742227bdd2ce447602a927ae6d60d59
Peter Lewis <plewis@calibrateconsulting.com>
2024-02-28 14:16:31 +0000

    install tailwind and add loading skeleton to results

--- src/app/components/Results/Results.tsx
+++ src/app/components/Results/Results.tsx
@@ -1,16 +1,19 @@
 "use client"
+import React, { Suspense } from 'react';
 import { SubmitData } from '@/app/types/Types'
 import moment from 'moment';
-import Result from './Result'
+import Result from './Result/Result'
+import LoadingSkeleton from './Result/Skeleton';
 
-export default function Results({ posts, query = '', isLoading }: { posts: SubmitData[], query: string, isLoading: boolean }) {
+export default function Results({ posts = [], query = '', isLoading }: { posts: SubmitData[], query: string, isLoading: boolean }) {
   console.log('posts', posts)
   return (
     <div className='results'>
-      { !posts.length && query === '' && isLoading && <h2>Loading...</h2>}
+      { posts.length === 0 && <LoadingSkeleton />}
+      {/* { !posts.length && query === '' && isLoading && <h2>Loading...</h2>}
       { !posts.length && query === '' && !isLoading && <h2>No posts</h2>}
       { !posts.length && query !== '' && <h2>No posts match your query</h2>}
-      { posts.length > 0 && <div className='number-of-results'><p>{posts.length} results</p></div> }
+      { posts.length > 0 && <div className='number-of-results'><p>{posts.length} results</p></div> } */}
       {/* <div className='remove-weatherspoons'>
         <label>Hide Weatherspoons
           <input type="checkbox" onChange={(e) => setHideSpoons(e.target.checked)} />
@@ -20,17 +23,18 @@ export default function Results({ posts, query = '', isLoading }: { posts: Submi
         { posts.length > 0 && posts.map((pub) => {
           const postDate = pub.date ? moment(pub.date).fromNow() : ''
           const { borough, drink, price, full_address, address, name, _id } = pub
-          return <Result
-                  key={_id}
-                  name={name}
-                  full_address={full_address}
-                  address={address}
-                  price={price}
-                  drink={drink}
-                  borough={borough}
-                  postDate={postDate}
-                  />
-        })}
+          return (
+            <Result
+                    key={_id}
+                    name={name}
+                    full_address={full_address}
+                    address={address}
+                    price={price}
+                    drink={drink}
+                    borough={borough}
+                    postDate={postDate}
+                    />
+        )})}
         </ul>
     </div>
   )
